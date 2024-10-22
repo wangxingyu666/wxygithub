f37b9878b87bd9509e59b0096a075bde8eb3c501
wangxingyu <wxy18115116156@163.com>
2024-10-22 11:56:58 +0800

    pinia模块学习

--- vue3-pinia/src/views/FinanceApp.vue
+++ vue3-pinia/src/views/FinanceApp.vue
@@ -34,7 +34,7 @@
 
 <script setup lang="ts">
 import { ref, computed } from "vue";
-import { useFinanceStore } from '../stores/useFinanceStore';
+import { useFinanceStore } from '../stores/useFinance';
 
 const financeStore = useFinanceStore();
 
